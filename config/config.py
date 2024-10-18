d628d3b045fb3601e38764c2c97e18487f46c844
shazib <shazibrahman@outlook.com>
2024-10-18 23:49:10 +0530

    refactored some codes .. I have no idea why

--- config/config.py
+++ config/config.py
@@ -1,40 +1,36 @@
 tags: list[str] = [
-    "nature",
-    "tree",
     "sky",
     "beach",
     "waterfall",
     "night",
     "mountain",
-    "garden",
     "sunset",
     "river",
     "landscape",
     "forest",
     "rain",
-    "bird",
     "ocean",
-    "butterfly",
-    "flower",
     "sunrise",
     "skyline",
     "northern lights",
-    "forest rain",
-    "garden flowers",
-    "sky stars",
+    "stars",
+    "coral reef",
+    "desktop background",
+    "jungle"
+
 ]
 config: dict[str:str | int | float | list] = {
     "check_internet_connection": True,
     "run_after_every_hour": 16,
     "not_delete_from_last_day": 7,
     "tags": tags,
-    "no_of_images_to_download": 2,
+    "no_of_images_to_download": 3,
     "force_download": False,
     "log_file": "wallpaper_updator.log",
     "lock_file": "wallpaper_updator.lock",
     "last_run_file": "last_run.txt",
     "timeout": 20,
-    "clear_images_data_after_days": 90,
+    "clear_images_data_after_days": 180,
 
 }
 
---
+++ decorators/add_tag_used_count.py
@@ -0,0 +1,91 @@
+from functools import wraps
+from pathlib import Path
+import ujson as json
+
+TAG_COUNT_JSON_FILE_PATH = Path(__file__).parent.parent.joinpath("data", "tags_counts.json")
+
+def read_tag_count_json():
+    """
+    Reads the tag count JSON file.
+
+    Returns:
+        dict: The tag count data.
+    """
+    with open(TAG_COUNT_JSON_FILE_PATH, "r") as file:
+        return json.load(file)
+
+
+def write_tag_count_json(data):
+    """
+    Writes the tag count data to the tag count JSON file.
+
+    Args:
+        data (dict): The tag count data.
+    """
+    with open(TAG_COUNT_JSON_FILE_PATH, "w") as file_to_write:
+        json.dump(data, file_to_write, indent=4)
+        
+        
+def choose_tag_with_least_usage(tags:list[str], tag_count:dict[str:str]):
+    """
+    Chooses a tag with the least usage count from the available tags.
+
+    Args:
+        tags (list[str]): The list of available tags.
+        tag_count (dict[str:str]): The dictionary containing the count of each tag.
+
+    Returns:
+        str: The tag with the least usage count.
+    """
+    least_used_tag = tags[0]
+    least_used_count = tag_count.get(tags[0], 0)
+    for tag in tags:
+        count = tag_count.get(tag, 0)
+        if count < least_used_count:
+            least_used_tag = tag
+            least_used_count = count
+    return least_used_tag
+
+def add_tag_used_count_return_tag_with_least_usage(func):
+    """
+    Decorator that increments the count of how many times a tag has been used.
+
+    Args:
+        func (callable): The function to be decorated.
+
+    Returns:
+        callable: The decorated function.
+    """
+
+    @wraps(func)
+    def wrapper(*args):
+        tags:list[str] = args[0]
+        tag_count = read_tag_count_json()
+        _remove_tag_count_if_not_in_tags(tag_count, tags)
+        tag_chosen = choose_tag_with_least_usage(tags, tag_count)
+        tag_count[tag_chosen] = tag_count.get(tag_chosen, 0) + 1
+        write_tag_count_json(tag_count)
+        return tag_chosen
+    return wrapper
+
+
+def _remove_tag_count_if_not_in_tags(tag_count:dict[str:str], tags:list[str]):
+    """
+    Removes tags from the tag count dictionary if they are not in the list of tags.
+
+    Args:
+        tag_count (dict[str:str]): The tag count dictionary.
+        tags (list[str]): The list of tags.
+    """
+    for tag in list(tag_count.keys()):
+        if tag not in tags:
+            del tag_count[tag]
+
+
+if __name__ == "__main__":
+    from config.config import tags
+    tag_count = read_tag_count_json()
+    _remove_tag_count_if_not_in_tags(tag_count, tags)
+    write_tag_count_json(tag_count)
+
+    ...
